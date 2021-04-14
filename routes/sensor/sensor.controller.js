358e9081b761689ff34d3be3f85372f53ca806a8
hanjuren <juren528@gmail.com>
2021-04-14 15:53:52 +0900

    평균 데이터 소수점 반올림

--- routes/sensor/sensor.controller.js
+++ routes/sensor/sensor.controller.js
@@ -2,7 +2,7 @@
 const dateFormat = require('dateformat');
 const messageController = require('../message/message.controller');
 const { Op } = require("sequelize");
-const { AptDong, AptHo, Sensor } = require('../../models');
+const { AptDong, AptHo, Sensor, sequelize } = require('../../models');
 
 
 // 시간대별로 만들기
@@ -11,51 +11,53 @@ exports.selectAll = async (req, res) => {
     // 올타임(99999), 1년(365), 6개월(182), 한달(30), 1주일(7), 하루(1)
     // 연도별
     try {
-        // let now = new Date('2021-02-01');
-        // console.log(now);
-        
-        // let { 
-        //     year = dateFormat(now, 'yyyy'),
-        //     month = dateFormat(now, 'mm'),
-        //     day = dateFormat(now, 'd'),
-        //     time, temperature, electricity, humidity, roomType} = req.body;
-        //     const enddate = Number.parseInt(month) + 1;
-        //     console.log(enddate);
-        
-        // const result = await Sensor.findAll({
-        //     where: {
-        //         created_at: {
-        //             [Op.between]: [dateFormat(`${year}-${month}-01 00:00:00`, 'yyyy-mm-dd HH:MM:ss'), dateFormat(`${year}-${enddate}-01 00:00:00`, 'yyyy-mm-dd HH:MM:ss')],                 
-        //             },
-        //     },
-        //     order: [['created_at', 'DESC']]
-        // });
-        // //const user = req.user;
-        // //messageController.sendMessage({user, result});
-        // res.json({result});
         let now = new Date();
-        // timeValue에 필요한 일수별로 받아오기
-        const { complex = "1단지", dong= "1동", ho= "101", timeValue="1"} = req.body;
-        const result = await AptDong.findOne({
-            attributes: ['apt_complex', 'apt_dong'],
+        console.log(now);
+        
+        let { 
+            year = dateFormat(now, 'yyyy'),
+            month = dateFormat(now, 'mm'),
+            day = dateFormat(now, 'd'),
+            time, temperature, electricity, humidity, roomType} = req.body;
+            const enddate = Number.parseInt(month) + 1;
+            
+        
+        const result = await Sensor.findAll({
             where: {
-                apt_complex: complex,
-                apt_dong: dong,
+                created_at: {
+                    [Op.between]: [dateFormat(`${year}-${month}-01 00:00:00`, 'yyyy-mm-dd HH:MM:ss'), dateFormat(`${year}-${enddate}-01 00:00:00`, 'yyyy-mm-dd HH:MM:ss')],                 
+                    },
             },
-            include: {
-                model: AptHo,
-                attributes: ['id'],
-                include: {
-                    model: Sensor,
-                    where: {
-                        created_at: {
-                            [Op.between]: [new Date(now - 86400000 * timeValue), now],
-                        }
-                    }
-                }
-            }
+            attributes: [ [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('humidity')), 2 ), 'sensoravg'] ],
+            order: [['created_at', 'DESC']]
         });
-        res.json(result);
+        //const user = req.user;
+        //messageController.sendMessage({user, result});
+        res.json({result});
+        // let now = new Date();
+        // // timeValue에 필요한 일수별로 받아오기
+        // const { complex = "1단", dong= "101", ho= "101", timeValue="1"} = req.body;
+        // const result = await AptDong.findOne({
+        //     attributes: ['apt_complex', 'apt_dong'],
+        //     where: {
+        //         apt_complex: complex,
+        //         apt_dong: dong,
+        //     },
+        //     include: {
+        //         model: AptHo,
+        //         attributes: ['id'],
+        //         include: {
+        //             model: Sensor,
+        //             where: {
+        //                 created_at: {
+        //                     [Op.between]: [new Date(now - 86400000 * timeValue), now],
+        //                 },
+        //             },
+        //             attributes: ['room_type', 'humidity', 'electricity'],
+        //         },
+        //     }
+        // });
+        // res.json(result);
     } catch (error) {
         console.log(error);
     }
