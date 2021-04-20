81ca01c0f886ae9fbff1d7d84cf180082655f757
hanjuren <juren528@gmail.com>
2021-04-20 18:31:32 +0900

    사용자 모델 생성

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            // 동
+            uid: {
+                type: Sequelize.STRING(100),
+                
+                allowNull: false,
+            },
+            uname: {
+                type: Sequelize.STRING(30),
+                allowNull: false,
+            }
+        }, {
+            sequelize,
+            timestamps: false,
+            underscored: true,
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+    static associate(db) {
+        db.User.belongsTo(db.AptHo, {foreignKey: 'apt_ho'}); // user 1 : 1 aptho
+    }
+};
