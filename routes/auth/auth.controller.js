93e394de53c8f13ec9584378870f460ea9d95bd6
SY <12tndbs12@naver.com>
2021-04-22 15:47:21 +0900

    사용자 선택 페이지

--- routes/auth/auth.controller.js
+++ routes/auth/auth.controller.js
@@ -1,8 +1,36 @@
-exports.getUserInfo = (req, res) => {
-        return res.render('./userinfo.html');
+const { AptDong, AptHo, sequelize } = require('../../models');
+
+exports.getUserInfo = async (req, res) => {
+    try {
+        const complex = await AptDong.findAll({
+            attributes: [
+                [sequelize.fn('DISTINCT', sequelize.col('apt_complex')), 'complex'],
+            ],
+        });
+        const dong = await AptDong.findAll({
+            attributes: [
+                [sequelize.fn('DISTINCT', sequelize.col('apt_dong')), 'dong'],
+            ]
+        });
+        const ho = await AptHo.findAll({
+            attributes: [
+                [sequelize.fn('DISTINCT', sequelize.col('apt_ho')), 'ho'],
+            ]
+        });
+        // const apt_complex = complex.map((aptComplex) => {
+        //     aptComplex._attributes;
+        // })
+        const aptComplex = JSON.stringify(complex);
+        const aptDong = JSON.stringify(dong);
+        const aptHo = JSON.stringify(ho);
+        
+        res.render('./userinfo.html', {aptComplex, aptDong, aptHo});
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 exports.patchUserInfo = (req, res) => {
     // 입력받은 단지, 동, 호 데이터베이스에 추가
-    return res.send('OK');
+    res.send('OK');
 }
