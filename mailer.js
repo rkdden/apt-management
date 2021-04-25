397f06ec2fb65c29c7577a05b5ad95eac15e67ef
Jonghwan Choi <jonghwanchoi@Jonghwanui-MacBookPro.local>
2021-04-25 10:59:42 +0900

    메일전송 기능 추가
    Signed-off-by: Jonghwan Choi <jonghwanchoi@Jonghwanui-MacBookPro.local>

---
+++ mailer.js
@@ -0,0 +1,31 @@
+var mailer = require('nodemailer');
+
+var mailSender = {
+    sendGmail: function(param) {
+        var transporter = mailer.createTransport({
+            service: "Gmail",
+            auth: {
+                user: "sadang.nodejs@gmail.com",
+                pass: "nodejs1234!"
+            }
+        });
+
+        var mailOptions = {
+            from: "sadang.nodejs@gmail.com",
+            to: param.toEmail,
+            subject: param.subject,
+            text: param.text
+        };
+
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Email sent: " + info.response);
+            }
+        });
+    }
+};
+
+module.exports = mailSender;
+
