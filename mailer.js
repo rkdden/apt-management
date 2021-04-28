cd78b2dc63914cc8f095ae513d2de0eb9c000c8c
Jonghwan Choi <jonghwanchoi@Jonghwanui-MacBookPro.local>
2021-04-28 22:02:31 +0900

    메일 전송시 파일 첨부하도록 수정
    Signed-off-by: Jonghwan Choi <jonghwanchoi@Jonghwanui-MacBookPro.local>

--- mailer.js
+++ mailer.js
@@ -1,30 +1,48 @@
-var mailer = require('nodemailer');
+const mailer = require('nodemailer');
 
-var mailSender = {
-    sendGmail: function(param) {
-        var transporter = mailer.createTransport({
-            service: "Gmail",
-            auth: {
-                user: "sadang.nodejs@gmail.com",
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
+const mailSender = {
+	sendGmail: function(param) {
+		const transporter = mailer.createTransport({
+			service: "Gmail",
+			auth: {
+				user: "sadang.nodejs@gmail.com",
+				pass: process.env.EMAIL_PASSWORD
+			}
+		});
 
-        var mailOptions = {
-            from: "sadang.nodejs@gmail.com",
-            to: param.toEmail,
-            subject: param.subject,
-            text: param.text
-        };
+		const mailOptions = {
+			from: "sadang.nodejs@gmail.com",
+			to: param.toEmail,
+			subject: param.subject,
+			text: param.text,
+			attachments: [
+				{
+					filename: param.name + "_temp.png",
+					path: __dirname + '/public/' + param.month + "/" + param.name + "_temp.png"
+				},
+				{
+					filename: param.name + "_humi.png",
+					path: __dirname + '/public/' + param.month + "/" + param.name + "_humi.png"
+				},
+				{
+					filename: param.name + "_watt.png",
+					path: __dirname + '/public/' + param.month + "/" + param.name + "_watt.png"
+				},
+			]
+		};
 
-        transporter.sendMail(mailOptions, function(error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log("Email sent: " + info.response);
-            }
-        });
-    }
+		transporter.sendMail(mailOptions, function(error, info) {
+			if (error) {
+				console.log(error);
+			} else {
+				console.log("Email sent: " + info.response);
+			}
+		});
+	},
+
+	setFileName: function(param) {
+		console.log(param);
+	},
 };
 
 module.exports = mailSender;
