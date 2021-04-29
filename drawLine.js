8197725c585e15c05bdba36b92928d3a55b1bb1e
Jonghwan Choi <jonghwanchoi@Jonghwanui-MacBookPro.local>
2021-04-29 21:10:13 +0900

    data에 각 달에 해당하는 디렉토리 생성
    Signed-off-by: Jonghwan Choi <jonghwanchoi@Jonghwanui-MacBookPro.local>

--- drawLine.js
+++ drawLine.js
@@ -1,10 +1,29 @@
 const ChartJsImage = require('chartjs-to-image');
+const fs = require('fs');
 
 const chart = new ChartJsImage();
 
 const days = function(month,year) {
 	return new Date(year, month, 0).getDate();
 };
+
+function get_Month() {
+	var today = new Date();
+
+	return today.getMonth() + 1;
+}
+
+function makeFolder() {
+	const folderName = './public/' + get_Month();
+
+	try {
+		if (!fs.existsSync(folderName)) {
+			fs.mkdirSync(folderName)
+		}
+	} catch (err) {
+		console.error(err)
+	}
+}
 
 async function saveChart(year, month, filename, type, data) {
 	const day = days(month, year);
@@ -18,6 +37,7 @@ async function saveChart(year, month, filename, type, data) {
 	});
 
 	// Write file to disk
+	makeFolder();
 	await chart.toFile(filename);
 }
 
@@ -25,5 +45,5 @@ module.exports = saveChart;
 
 /* usage
 const data = [...Array(100).keys()]; => data for temperature or humidity or watt
-saveChart(2021, 4, "./newchart.png", "temp", data);
+saveChart(2021, 4, "./data/4/newchart.png", "temp", data);
 */
