cfbbb29964141a6b450923cb294b2f2db322ab65
SY <12tndbs12@naver.com>
2021-05-01 17:29:14 +0900

    메일 차트 수정

--- service/email/emailService.js
+++ service/email/emailService.js
@@ -1,13 +1,10 @@
 const { AptHo, AptDong } = require('../../models');
 
-module.exports = {
-    async aptFind() {
-        const apt = await AptHo.findAll({
-                include: {
-                    model: AptDong
-                },
-        })
-        return apt;
-        
-    }   
-};
+exports.aptFind = async () =>{
+    const apt = await AptHo.findAll({
+            include: {
+                model: AptDong
+            },
+    })
+    return apt;
+} 
